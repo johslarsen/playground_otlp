67c6b8c662b63fa0976fe4acfb2333dc06cacb30
Johannes Larsen <mail@johslarsen.net>
2024-05-09 19:11:03 +0200

    Extend example to deal with traces

--- example.cpp
+++ example.cpp
@@ -1,6 +1,7 @@
 #include "example.h"
 
 #include <opentelemetry/logs/provider.h>
+#include <opentelemetry/trace/provider.h>
 
 // needs to be lazily initialized to happen after SetLoggerProvider
 static auto& log() {
@@ -9,6 +10,18 @@ static auto& log() {
   return log;
 }
 
+// needs to be lazily initialized to happen after SetTracerProvider
+static auto& trace() {
+  // who knows if this should be thread-local?
+  static auto log = opentelemetry::trace::Provider::GetTracerProvider()->GetTracer("example_name", "example_lib");
+  return log;
+}
+
 void example::foo() {
-  log()->Info("body");
+  auto unrelated = trace()->StartSpan("other");
+  unrelated->SetAttribute("bar", 42);
+  opentelemetry::trace::Scope parent(trace()->StartSpan("parent_scope"));
+  opentelemetry::trace::Scope child(trace()->StartSpan("child_scope"));
+  log()->Info("implicitly tagged with child_scope");
+  log()->Info("explicitly tagged with unrelated", unrelated->GetContext());
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <opentelemetry/exporters/ostream/log_record_exporter.h>
+#include <opentelemetry/exporters/ostream/span_exporter.h>
 #include <opentelemetry/logs/provider.h>
 #include <opentelemetry/sdk/logs/logger_provider_factory.h>
 #include <opentelemetry/sdk/logs/simple_log_record_processor.h>
+#include <opentelemetry/sdk/trace/simple_processor.h>
+#include <opentelemetry/sdk/trace/tracer_provider_factory.h>
+#include <opentelemetry/trace/provider.h>
 
 #include "example.h"
 
 int main() {
+  using opentelemetry::exporter::trace::OStreamSpanExporter;
+  using opentelemetry::sdk::trace::SimpleSpanProcessor;
+  using opentelemetry::sdk::trace::TracerProviderFactory;
+  opentelemetry::trace::Provider::SetTracerProvider(
+      TracerProviderFactory::Create(
+          std::make_unique<SimpleSpanProcessor>(
+              std::make_unique<OStreamSpanExporter>())));
+
   using opentelemetry::exporter::logs::OStreamLogRecordExporter;
   using opentelemetry::sdk::logs::LoggerProviderFactory;
   using opentelemetry::sdk::logs::SimpleLogRecordProcessor;
