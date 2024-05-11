fe9162c287bc44f434990e5bc00f45bd765d4a9a
Johannes Larsen <mail@johslarsen.net>
2024-05-11 15:04:14 +0200

    Add external/abseil-cpp submodule

--- update_needed_submodules.sh
+++ update_needed_submodules.sh
@@ -2,3 +2,4 @@
 git submodule update --init external/opentelemetry-cpp/
 (cd external/opentelemetry-cpp/; git submodule update --init third_party/prometheus-cpp/)
 (cd external/opentelemetry-cpp/third_party/prometheus-cpp/; git submodule update --init 3rdparty/civetweb/)
+git submodule update --init external/abseil-cpp/
